4a4eec1c2b92d66249c78074149ca5615a5b12da
duy <ddao0892@gmail.com>
2021-01-12 13:54:13 -0700

    problem01

--- problems/01-constructor-function.js
+++ problems/01-constructor-function.js
@@ -17,35 +17,39 @@ Node.js with the examples below. Use the command:
 
 Examples:
 
-const message1 = new SMS('555-111-1111', '555-222-2222', 'Test message one');
-console.log(message1);
 
 // Should print...
 
 // SMS {
-//   recipient: '555-111-1111',
-//   sender: '555-222-2222',
-//   text: 'Test message one'
-// }
+  //   recipient: '555-111-1111',
+  //   sender: '555-222-2222',
+  //   text: 'Test message one'
+  // }
 
-const message2 = new SMS('555-333-3333', '555-444-4444', 'Test message two');
-console.log(message2);
+  const message2 = new SMS('555-333-3333', '555-444-4444', 'Test message two');
+  console.log(message2);
 
-// Should print...
+  // Should print...
 
-// SMS {
-//   recipient: '555-333-3333',
-//   sender: '555-444-4444',
-//   text: 'Test message two'
-// }
+  // SMS {
+    //   recipient: '555-333-3333',
+    //   sender: '555-444-4444',
+    //   text: 'Test message two'
+    // }
 
-***********************************************************************/
+    ***********************************************************************/
 
+   function SMS (recipient, sender, text) {
+     this.recipient = recipient;
+     this.sender = sender;
+     this.text = text;
+    }
 
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
-try {
-  module.exports = SMS;
+    const message1 = new SMS('555-111-1111', '555-222-2222', 'Test message one');
+    console.log(message1);
+    /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+    try {
+      module.exports = SMS;
 } catch {
   module.exports = null;
 }
